794ddbed93314bdb5e24c7c8a5a71be16e175e8f
Gustavo Pedreros <gustavo.pedreros@gmail.com>
2026-08-21 21:29:24 -0400

    build(app): align JVM target to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,12 +28,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     buildFeatures {
         compose = true
     }
+}
+
+kotlin {
+    jvmToolchain(libs.versions.jvmTarget.get().toInt())
 }
 
 dependencies {
