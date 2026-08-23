b2d180193048f01727142562e790a722118729be
Gustavo Pedreros <gustavo.pedreros@gmail.com>
2026-08-23 10:19:13 -0400

    chore(core-database): scaffold empty module

---
+++ core/database/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    id("fakestore.android.library")
+    id("fakestore.android.room")
+    id("fakestore.android.hilt")
+    id("fakestore.testing")
+}
+
+android {
+    namespace = "cl.gus.labs.fakestore.core.database"
+}
+
+dependencies {
+}
