1b8c23b6f90939ccd8226dc0499c26c5bf685826
Gustavo Pedreros <gustavo.pedreros@gmail.com>
2026-08-23 02:51:36 -0400

    build(build-logic): wire the composite build

---
+++ build-logic/convention/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    `kotlin-dsl`
+}
+
+group = "cl.gus.labs.fakestore.buildlogic"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(libs.versions.jvmTarget.get().toInt())
+    }
+}
+
+dependencies {
+    compileOnly(libs.android.gradle.plugin)
+    compileOnly(libs.kotlin.gradle.plugin)
+    compileOnly(libs.compose.gradle.plugin)
+    compileOnly(libs.ksp.gradle.plugin)
+    compileOnly(libs.hilt.gradle.plugin)
+}
