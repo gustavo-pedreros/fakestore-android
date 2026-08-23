2a0b6bd4ea819fddc82cc9c74cb7861d13990df1
Gustavo Pedreros <gustavo.pedreros@gmail.com>
2026-08-23 00:51:29 -0400

    feat(app): add a temporary catalog screen for the design system

--- app/src/main/java/cl/gus/labs/fakestore/MainActivity.kt
+++ app/src/main/java/cl/gus/labs/fakestore/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.systemBars
@@ -12,8 +11,8 @@ import androidx.compose.foundation.layout.windowInsetsPadding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import cl.gus.labs.fakestore.core.designsystem.molecule.FsListTopBar
 import cl.gus.labs.fakestore.core.designsystem.theme.FakeStoreTheme
+import cl.gus.labs.fakestore.demo.DesignSystemCatalog
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,13 +24,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .windowInsetsPadding(WindowInsets.systemBars),
-                    ) {
-                        FsListTopBar(onFavoritesClick = {})
-                    }
+                    DesignSystemCatalog(
+                        modifier = Modifier.windowInsetsPadding(WindowInsets.systemBars),
+                    )
                 }
             }
         }
